0d539557c28c1a585ce0b8f4ad997d4fd2e6c581
simycck <chunkhong@hotmail.com>
2018-02-08 15:32:31 +0800

    Code change

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -3,7 +3,7 @@ public class MyFirstJavaProgram {
   public static void main(String []args){
     
     for(int i=1;i<=10;i++){
-    System.out.println("Hello World");
+    System.out.println("Hello World" + i);
     }
   }
 }
