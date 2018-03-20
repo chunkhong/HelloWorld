83022dc77db9865488c0f01f2ceeb5430c520b4e
simycck <chunkhong@hotmail.com>
2018-03-20 15:57:40 +0800

    testing

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -3,7 +3,7 @@ public class MyFirstJavaProgram {
   public static void main(String []args){
     
     for(int i=1;i<=10;i++){
-      System.out.println("Hello World " + i + " time " + i);
+      System.out.println("Hello World " + i + " time " + i + " How are you Amir?");
     }
   }
 }
